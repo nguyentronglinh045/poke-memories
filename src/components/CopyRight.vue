47d023be8c16b730a0cd083044e5cb7dd12ddf6b
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-06-25 18:24:19 +0700

    add links git

--- src/components/CopyRight.vue
+++ src/components/CopyRight.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="copyright">
     <p>
-      This game make by Vue 3 for begginer - <a href="#">view source here</a>
+      This game make by Vue 3 for begginer NguyenTrongLinh045 -
+      <a href="https://github.com/nguyentronglinh045/poke-memories"
+        >view source here
+      </a>
     </p>
   </div>
 </template>
